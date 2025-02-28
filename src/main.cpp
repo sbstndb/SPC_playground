aa96fe404173f1bd8390f7f2efe8b74966c6a2f3
sbstndb/sbstndbs <sebastiendubois4@gmail.com>
2025-02-28 15:08:57 +0100

    Improve performance
    	-	add branchless stencil computation

--- src/main.cpp
+++ src/main.cpp
@@ -97,23 +97,23 @@ public:
 template <int Order>
 struct LaplacianStencil;
 
-/**
+
 template <>
 struct LaplacianStencil<5> {
-    static double compute(const std::vector<double>& grid, int x, int y, int width, int height) {
+    static double compute(const std::vector<double>& grid, int x, int y, int width, int height, int ghost_size) {
         double sum = 0.0;
-        int idx = y * width + x;
+        int idx = y * (width+2*ghost_size) + x + ghost_size;
         sum -= 4 * grid[idx];
-        double up     = grid[(y - 1) * width + x] * (y > 0);
-        double down   = grid[(y + 1) * width + x] * (y < height - 1);
-        double left   = grid[y * width + (x - 1)] * (x > 0);
-        double right  = grid[y * width + (x + 1)]  * (x < width - 1);
+        double up     = grid[(y - 1) * (width+2*ghost_size) + x + ghost_size] * (y > ghost_size);
+        double down   = grid[(y + 1) * (width+2*ghost_size) + x + ghost_size] * (y < height - 1 +2*ghost_size);
+        double left   = grid[y * (width+2*ghost_size) + (x - 1) + ghost_size] * (x > ghost_size);
+        double right  = grid[y * (width+2*ghost_size) + (x + 1) + ghost_size]  * (x < width - 1 + 2*ghost_size);
 	sum += up+down+left+right ; 
         return sum;
     }
 };
-**/
 
+/**
 template <>
 struct LaplacianStencil<5> {
     static double compute(const std::vector<double>& grid, int x, int y, int width, int height, int ghost_size) {
@@ -127,7 +127,7 @@ struct LaplacianStencil<5> {
         return sum;
     }
 };
-
+**/
 
 
 
